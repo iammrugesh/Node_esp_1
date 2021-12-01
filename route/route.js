c67ee1cf7fc065e0f0fc2c175579468b11aaf806
Mrugesh Patel <patelmrugesh5@gmail.com>
2021-12-01 09:35:53 +0530

    first

--- route/route.js
+++ route/route.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now());
@@ -7,15 +8,17 @@ router.use(function timeLog (req, res, next) {
 });
 
 router.get('/', function (req, res) {
-  res.sendFile('./login.html', {root: __dirname })
-//   res.send('login here');
+  res.sendFile(path.join(__dirname, '../views', 'login.html'));
 });
 
 router.post('/', function (req, res) {
-  // res.render('the_template', { name: req.body.name });
   console.log(req.body.name);
   console.log(req.body.password);
   res.redirect('/');
 });
 
+router.use((req, res , next) => {
+  console.log('Error');
+  res.sendFile(path.join(__dirname, '../views', 'error.html'));
+});
 module.exports = router;
